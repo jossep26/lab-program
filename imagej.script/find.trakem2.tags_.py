7894af7b0a97736f9e7e196b472a4c0798cd1f52
singingstars <singingstars@163.com>
2013-11-21 19:48:35 +0800

    add fields for node identification

--- imagej.script/find.trakem2.tags_.py
+++ imagej.script/find.trakem2.tags_.py
@@ -4,7 +4,7 @@
 from ini.trakem2.display import AreaList, Display, AreaTree, Connector
 import csv
 
-header = ['type', 'parentId', 'parentName', 'shortMeaningfulTitle', 'node', 'tag']
+header = ['type', 'parentId', 'node_degree', 'layer','parentName', 'shortMeaningfulTitle', 'node', 'tag']
 foundtags = [header]
 
 areatrees = Display.getFront().getLayerSet().getZDisplayables(AreaTree)
@@ -19,7 +19,7 @@ for areatree in areatrees:
         if tags is None:
             continue
         for tag in tags:
-            foundtags.append( ['areatrea', areatree.getId(), areatree.getTitle(), areatree.getProject().getShortMeaningfulTitle(areatree), str(node), str(tag)] )
+            foundtags.append( ['areatrea', areatree.getId(), node.computeDegree(), node.getLayer().getId(), areatree.getTitle(), areatree.getProject().getShortMeaningfulTitle(areatree), str(node), str(tag)] )
 
 connectors = Display.getFront().getLayerSet().getZDisplayables(Connector)
 for connector in connectors:
@@ -31,7 +31,7 @@ for connector in connectors:
         if tags is None:
             continue
         for tag in tags:
-            foundtags.append( ['connector', connector.getId(), connector.getTitle(), connector.getProject().getShortMeaningfulTitle(connector), str(node), str(tag)] )
+            foundtags.append( ['connector', connector.getId(), node.computeDegree(), node.getLayer().getId(), connector.getTitle(), connector.getProject().getShortMeaningfulTitle(connector), str(node), str(tag)] )
 
 outfile = open('test.csv','wb')
 writer = csv.writer(outfile)
