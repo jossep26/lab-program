8b1ed640873787a25e16b431da58ad2dfe682882
singingstars <singingstars@163.com>
2013-12-28 20:07:20 +0800

    add correct method for finding connectors

--- imagej.script/find.trakem2.node.connector_.py
+++ imagej.script/find.trakem2.node.connector_.py
@@ -1,16 +1,17 @@
-# output a list of nodes associated with connectors labeled by hand
+# output a list of nodes associated with connectors
 
-from ini.trakem2.display import AreaList, Display, AreaTree, Connector
+from ini.trakem2.display import AreaList, Display, AreaTree, Connector, Tree
 from fiji.geom import AreaCalculations
 import csv
 from jarray import array
 from java.awt.geom import Area
 from java.awt import Rectangle
+import re
 
 header = ['neuron', 'neurite', 'areatreeId', 'nodeId', 'branch', 'layer', 'x', 'y', 'z', 'area', 'nInputs', 'nOutputs']
 foundNeuriteNodes = [header]
 
-project = Project.getProjects().get(1)
+project = Project.getProjects().get(0)
 projectRoot = project.getRootProjectThing()
 
 # recursive function to assign nodes with 'branch id' to mark different linear segments
@@ -26,9 +27,41 @@ def markTreeBranch(node):
         branchId += 1
         markTreeBranch(childnd)
 
+def findConnectorsInTree(tree):
+    # returns a dict of two values, each contains a dict of node versus connectors
+    outgoing = {}
+    incoming = {}
+    las = tree.getLayerSet()
+    cal = las.getCalibration()
+    at = tree.getAffineTransform()
+    root = tree.getRoot()
+    if root is None:
+        return
+    for nd in root.getSubtreeNodes():
+        # copied from TrakEM2_Scripting and Tree.java
+        la = nd.getLayer()
+        area = nd.getArea().clone()
+        area.transform(at)
+        cs = las.findZDisplayables(Connector, la, area, False, False)
+        if cs.isEmpty():
+            continue
+        for connector in cs:
+            # print 'found', connector
+            if connector.intersectsOrigin(area, la):
+                if nd in outgoing:
+                    outgoing[nd].append(connector)
+                else:
+                    outgoing[nd] = [connector]
+            else:
+                if nd in incoming:
+                    incoming[nd].append(connector)
+                else:
+                    incoming[nd] = [connector]
+    return {'outgoing':outgoing, 'incoming':incoming}
+
 neurites = projectRoot.findChildrenOfTypeR("neurite")
 for neurite in neurites:
-    if 'apla_' not in neurite.getTitle():
+    if 'test_' not in neurite.getTitle():
         continue
     areatrees = neurite.findChildrenOfTypeR("areatree")
     for areatree in areatrees:
@@ -37,6 +70,7 @@ for neurite in neurites:
         if root is None:
             break
 
+        cTable = findConnectorsInTree(areatree) # auto find connectors
         branchId = 1
         branchTable = {}
         markTreeBranch(root)
@@ -60,6 +94,18 @@ for neurite in neurites:
             if 0 == (nOutputs + nInputs):
                 continue
                     
+            # auto find synapse
+            nAutoOut = 0
+            nAutoIn = 0
+            outputs = cTable['outgoing']
+            inputs = cTable['incoming']
+            if nd in outputs:
+                nAutoOut = len(outputs[nd])
+            if nd in inputs:
+                nAutoIn = len(inputs[nd])
+            if nOutputs != nAutoOut or nInputs != nAutoIn:
+                print nd, 'MAN', nOutputs, 'outputs', nInputs, 'inputs'
+                print nd, 'AUTO', nAutoOut, 'outputs', nAutoIn, 'inputs'
             # get node coordinates, from tut on web
             fp = array([nd.getX(), nd.getY()], 'f')
             affine.transform(fp, 0, fp, 0, 1)
@@ -82,7 +128,7 @@ for neurite in neurites:
             nodeData = [neurite.getParent().getTitle(), neurite.getTitle(), areatree.getId(), nd.getId(), branch, ndLayerIndex, x, y, z, ndArea, nInputs, nOutputs]
             foundNeuriteNodes.append(nodeData)
 
-outfile = open('neuritesA.csv','wb')
+outfile = open('neuritesConnectors01.csv','wb')
 writer = csv.writer(outfile)
 writer.writerows(foundNeuriteNodes)
 outfile.close()
