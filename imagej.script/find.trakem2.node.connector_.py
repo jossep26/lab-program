ec908f8dc9d95f9b9be28edbd0ecb407b025a6bb
singingstars <singingstars@163.com>
2013-12-30 22:27:57 +0800

    add find connection trees function

--- imagej.script/find.trakem2.node.connector_.py
+++ imagej.script/find.trakem2.node.connector_.py
@@ -1,6 +1,6 @@
 # output a list of nodes associated with connectors
 
-from ini.trakem2.display import AreaList, Display, AreaTree, Connector, Tree
+from ini.trakem2.display import AreaList, Display, AreaTree, Connector, Tree, Treeline
 from fiji.geom import AreaCalculations
 import csv
 from jarray import array
@@ -8,12 +8,7 @@ from java.awt.geom import Area
 from java.awt import Rectangle
 import re
 
-header = ['neuron', 'neurite', 'areatreeId', 'nodeId', 'branch', 'layer', 'x', 'y', 'z', 'area', 'nInputs', 'nOutputs']
-foundNeuriteNodes = [header]
-
-project = Project.getProjects().get(0)
-projectRoot = project.getRootProjectThing()
-
+##### tools #####
 # recursive function to assign nodes with 'branch id' to mark different linear segments
 branchId = 0
 branchTable = {}
@@ -27,8 +22,16 @@ def markTreeBranch(node):
         branchId += 1
         markTreeBranch(childnd)
 
+def isTree(x):
+    if isinstance(x, Connector):
+        return False
+    return isinstance(x, Treeline) or isinstance(x, AreaTree)
+ 
 def findConnectorsInTree(tree):
     # returns a dict of two values, each contains a dict of node versus connectors
+    if not isTree(tree):
+        print "error: input is neither Treeline or AreaTree"
+        return
     outgoing = {}
     incoming = {}
     las = tree.getLayerSet()
@@ -59,6 +62,70 @@ def findConnectorsInTree(tree):
                     incoming[nd] = [connector]
     return {'outgoing':outgoing, 'incoming':incoming}
 
+def findConnectedTree(tree):
+    # returns a dict of two values, each contains a dict of node versus tree
+    if isinstance(tree, AreaTree):
+        fixAreatreeArea(tree)
+    outputs = {}
+    inputs = {}
+    las = tree.getLayerSet()
+    cal = las.getCalibration()
+    at = tree.getAffineTransform()
+    root = tree.getRoot()
+    if root is None:
+        return
+    for nd in root.getSubtreeNodes():
+        # copied from TrakEM2_Scripting and Tree.java
+        la = nd.getLayer()
+        area = nd.getArea().clone()
+        area.transform(at)
+        cs = las.findZDisplayables(Connector, la, area, False, False)
+        if cs.isEmpty():
+            continue
+        for connector in cs:
+            # print 'found', connector
+            if connector.intersectsOrigin(area, la):
+                # outgoing connector
+                t = filter(isTree, [t for ts in connector.getTargets() for t in ts])
+                t = set(t)
+                # print 'targets', t
+                if nd in outputs:
+                    outputs[nd] = outputs[nd].union(t)
+                else:
+                    outputs[nd] = t
+            else:
+                o = filter(isTree, connector.getOrigins())
+                o = set(o)
+                # print 'origin', o
+                if nd in inputs:
+                    inputs[nd] = inputs[nd].union(o)
+                else:
+                    inputs[nd] = o
+    return {'outputs':outputs, 'inputs':inputs}
+
+def fixAreatreeArea(areatree):
+    # Append a 1-pixel square area for nodes without any area
+    # this will fix using findZDisplayables for finding areatree, 
+    #     including connector.getTargets(), connector.getOrigins() etc.
+    if not isinstance(areatree, AreaTree):
+        print "Error: input must be an AreaTree"
+        return
+    root = areatree.getRoot()
+    if root is None:
+        return
+    for nd in root.getSubtreeNodes():
+        a = nd.getArea()
+        a.add(Area(Rectangle(int(nd.getX()), int(nd.getY()), 1, 1)))
+        nd.setData(a)
+
+##### tools end #####
+
+header = ['neuron', 'neurite', 'areatreeId', 'nodeId', 'branch', 'layer', 'x', 'y', 'z', 'area', 'nInputs', 'nOutputs']
+foundNeuriteNodes = [header]
+
+project = Project.getProjects().get(0)
+projectRoot = project.getRootProjectThing()
+
 neurites = projectRoot.findChildrenOfTypeR("neurite")
 for neurite in neurites:
     if 'test_' not in neurite.getTitle():
@@ -71,6 +138,7 @@ for neurite in neurites:
             break
 
         cTable = findConnectorsInTree(areatree) # auto find connectors
+        print findConnectedTree(areatree)
         branchId = 1
         branchTable = {}
         markTreeBranch(root)
@@ -128,8 +196,7 @@ for neurite in neurites:
             nodeData = [neurite.getParent().getTitle(), neurite.getTitle(), areatree.getId(), nd.getId(), branch, ndLayerIndex, x, y, z, ndArea, nInputs, nOutputs]
             foundNeuriteNodes.append(nodeData)
 
-outfile = open('neuritesConnectors01.csv','wb')
-writer = csv.writer(outfile)
-writer.writerows(foundNeuriteNodes)
-outfile.close()
-            
+# outfile = open('neuritesConnectors01.csv','wb')
+# writer = csv.writer(outfile)
+# writer.writerows(foundNeuriteNodes)
+# outfile.close()
