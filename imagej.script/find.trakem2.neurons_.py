83198cde1f9c13d8ec6263c1e64d20a032fe0200
singingstars <singingstars@163.com>
2014-08-12 17:17:47 +0800

    add neurite length

--- imagej.script/find.trakem2.neurons_.py
+++ imagej.script/find.trakem2.neurons_.py
@@ -6,8 +6,38 @@
 
 from ini.trakem2.display import AreaList, Display, AreaTree, Connector
 import csv
+from jarray import array
 
-header = ['neurite', 'neuron', 'areatreeIds', 'nNodes']
+def getNodeXYZ(nd, cal, at):
+    # get node coordinates, from tut on web
+    fp = array([nd.getX(), nd.getY()], 'f')
+    at.transform(fp, 0, fp, 0, 1)
+    x = fp[0] * cal.pixelWidth
+    y = fp[1] * cal.pixelHeight
+    z = nd.getLayer().getZ() * cal.pixelWidth  # a TrakEM2 oddity
+    return [x, y, z]
+
+def getTreeDistanceTable(tree):
+    # calculate and store each node's distance to its parent node
+    dTable = {}
+    layerset = tree.getLayerSet()
+    calibration = layerset.getCalibration()
+    affine = tree.getAffineTransform()
+    root = tree.getRoot()
+    if root is None:
+        return
+    for nd in root.getSubtreeNodes():
+        x, y, z = getNodeXYZ(nd, calibration, affine)
+        if nd.getParent() is None:
+            # root
+            xp, yp, zp = [x, y, z]
+        else:
+            xp, yp, zp = getNodeXYZ(nd.getParent(), calibration, affine)
+        d = ((x-xp)**2 + (y-yp)**2 + (z-zp)**2) ** (0.5)
+        dTable[nd] = d
+    return dTable
+
+header = ['neurite', 'neuron', 'areatreeIds', 'nNodes', 'length']
 foundNeurons = [header]
 
 # get the first open project, project root, and 'drosophila_brain'
@@ -22,14 +52,17 @@ for neuron in neurons:
         areatrees = neurite.findChildrenOfType("areatree")
         nNodes = 0
         areatreeIds = []
+        length = 0
         for areatree in areatrees:
             areatree = areatree.getObject()
             areatreeIds.append(areatree.getId())
+            dt = getTreeDistanceTable(areatree)
+            length += sum([d for nd, d in dt.iteritems()])
             root = areatree.getRoot()
             if root is None:
                 continue
             nNodes += len(root.getSubtreeNodes())
-        foundNeurons.append([neurite.getTitle(), neuron.getTitle(), areatreeIds, nNodes])
+        foundNeurons.append([neurite.getTitle(), neuron.getTitle(), areatreeIds, nNodes, length])
 
 outfile = open('neurons.csv','wb')
 writer = csv.writer(outfile)
