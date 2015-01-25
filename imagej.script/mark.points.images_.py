2899350f9ca7251276c28f85cd48e2b8b61b15fc
singingstars <singingstars@163.com>
2015-01-25 21:05:31 +0800

    add iterator, still alpha

--- imagej.script/mark.points.images_.py
+++ imagej.script/mark.points.images_.py
@@ -1,23 +1,27 @@
 from ij import IJ, ImagePlus, ImageStack
+from ij.gui import Overlay, Roi, PointRoi, Line, TextRoi
+from ij.io import DirectoryChooser
 from java.awt.event import KeyEvent, KeyAdapter, MouseAdapter
-from ij.gui import Overlay, Roi, Line, TextRoi
 from java.lang import String
+from java.awt import Color
 import os
 import random
 import math
 import csv
 
-imgPath = "d:\\tmp\\pointon\\2012-09-18 221x5905_Image011.tif"
-
 def drawEnd(imp, keyEvent):
+    if keyEvent.getKeyCode() == KeyEvent.VK_S:
+        print "save!"
+        keyEvent.consume()
+        print keyEvent.isConsumed()
     if keyEvent.getKeyCode() == KeyEvent.VK_SPACE:
         print "space!"
-        processPoints(imp)
+        
     if keyEvent.getKeyCode() == KeyEvent.VK_PERIOD:
         print "next!"
         savePoints(imp)
         imp.close()
-        prepareNewImage(imgPath)
+        prepareNewImage(imgIt)
 
 class ListenToDrawEnd(KeyAdapter):
     def keyPressed(this, event):
@@ -59,6 +63,8 @@ def drawLines(imp):
         if yi < y0:
             yt -= textOffset
         lineRoi = Line(x0, y0, xi, yi)
+        lineRoi.setStrokeWidth(1)
+        lineRoi.setStrokeColor(Color(255,255,0))
         dTextRoi = TextRoi(xt, yt, dText)
         ol.add(lineRoi)
         ol.add(dTextRoi)
@@ -82,8 +88,11 @@ def savePoints(imp):
     writer.writerows([out])
     outfile.close()
 
-
-def prepareNewImage(imgPath):
+def prepareNewImage(imgIt, direction=None):
+    if direction == 'prev':
+        imgPath = imgIt.prev()
+    else:
+        imgPath = imgIt.next()
     imp = IJ.openImage(imgPath)
     imp.show()
 
@@ -93,9 +102,47 @@ def prepareNewImage(imgPath):
     win = imp.getWindow()
     if win:
         canvas = win.getCanvas()
+        # override key listeners
+        kls = canvas.getKeyListeners()
+        print len(kls)
+        map(canvas.removeKeyListener, kls)
         canvas.addKeyListener(lEnd)
+        map(canvas.addKeyListener, kls)
         canvas.addMouseListener(lAdd)
     print "ding!"
+
+class twoWayCircularIterator(object):
+    def __init__(self, l):
+        # l should be a list
+        self.l = l
+        self.n = len(l)
+        self.i = 0
+    def next(self):
+        cur = self.l[self.i]
+        self.i += 1
+        if self.i >= self.n:
+            self.i = 0
+        return cur
+    def prev(self):
+        cur = self.l[self.i]
+        self.i -= 1
+        if self.i < 0:
+            self.i = self.n - 1
+        return cur
+
+
+#######################
+imgPath = "d:\\tmp\\pointon\\2012-09-18 221x5905_Image011.tif"
+imgDir = DirectoryChooser("Chose Directory of Images").getDirectory()
+imgPaths = []
+if imgDir:
+    for root, directories, filenames in os.walk(imgDir):
+        for filename in filenames:
+            # Skip non-TIF files
+            if not filename.endswith(".tif"):
+                continue
+            imgPaths.append(os.path.join(root, filename))
+imgIt = twoWayCircularIterator(imgPaths)
 
 header = [['x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'pixel_width', 'unit', 'image_name']]
 outfn = 'points.csv'
@@ -106,5 +153,6 @@ writer.writerows(header)
 outfile.close()
 
 IJ.setTool("multipoint")
+PointRoi.setDefaultSize(3)
 
-prepareNewImage(imgPath)
+prepareNewImage(imgIt)
