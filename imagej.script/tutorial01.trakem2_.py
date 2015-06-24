360e73ca8e166bc8dc4536b0b16574b2547f51b8
singingstars <singingstars@163.com>
2015-06-24 20:47:05 +0800

    add tut

---
+++ imagej.script/tutorial01.trakem2_.py
@@ -0,0 +1,33 @@
+from ini.trakem2 import Project
+import csv
+
+foundNeurites = []
+
+project = Project.getProjects().get(0)
+projectRoot = project.getRootProjectThing()
+neurites = projectRoot.findChildrenOfTypeR("neurite")
+for neurite in neurites:
+    print("-----")
+    print(neurite)
+    print(neurite.getTitle())
+    foundNeurites.append([neurite.getTitle()])
+
+outfile = open('test.csv','wb')
+writer = csv.writer(outfile)
+writer.writerows(foundNeurites)
+outfile.close()
+
+#### Exercises:
+# 0. Run the code and check what happened.
+# 1. Read and explain to yourself the meaning of each line.
+# 2. On line 6, Project.getProjects().get(0), what's the meaning of "."?
+#    (It's about Java classes' methods, you can search the web, preferably Google.)
+# 3. Comment out line 10, or line 11, or line 12, what happened?
+#    (You can search what is a comment and how to use it in Python.)
+# 4. On line 13, what is the meaning of "."? What will happen if you delete the "[]"?
+#    (It's about Python class methods, again search for it.)
+#    (After you omit the [], check the output test.csv file.)
+# 5. On line 8, what will happen if you change the 2nd "neurite" (the one in double quotes)
+#    to "synapse" (don't omit the double quotes)? What will happen if you change the first occurence?
+# 6. On line 12, what will happen if you change neurite.getTitle() to neurite.getId() ?
+#
