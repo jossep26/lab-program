f2bb0eba07c16358ac5ba7d2b916f5c4861e815d
singingstars <singingstars@163.com>
2015-06-29 17:32:08 +0800

    update line no. in exercise

--- imagej.script/tutorial01.trakem2_.py
+++ imagej.script/tutorial01.trakem2_.py
@@ -23,14 +23,14 @@ outfile.close()
 #### Exercises:
 # 0. Run the code and check what happened.
 # 1. Read and explain to yourself the meaning of each line.
-# 2. On line 6, Project.getProjects().get(0), what's the meaning of "."?
+# 2. On line 9, Project.getProjects().get(0), what's the meaning of "."?
 #    (It's about Java classes' methods, you can search the web, preferably Google.)
-# 3. Comment out line 10, or line 11, or line 12, what happened?
+# 3. Comment out line 13, or line 14, or line 15, what happened?
 #    (You can search what is a comment and how to use it in Python.)
-# 4. On line 13, what is the meaning of "."? What will happen if you delete the "[]"?
+# 4. On line 16, what is the meaning of "."? What will happen if you delete the "[]"?
 #    (It's about Python class methods, again search for it.)
 #    (After you omit the [], check the output test.csv file.)
-# 5. On line 8, what will happen if you change the 2nd "neurite" (the one in double quotes)
+# 5. On line 11, what will happen if you change the 2nd "neurite" (the one in double quotes)
 #    to "synapse" (don't omit the double quotes)? What will happen if you change the first occurence?
-# 6. On line 12, what will happen if you change neurite.getTitle() to neurite.getId() ?
+# 6. On line 15, what will happen if you change neurite.getTitle() to neurite.getId() ?
 #
